f6c74cfada65d3f846ef721d53904678f3e88f33
Nemanja Spasic <59572186+nspa4921@users.noreply.github.com>
2022-01-16 15:23:30 +0100

    app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import './App.css';
+import harbaData from './data/habraApI.json';
+import { Modal } from './components/react-modal/modal';
+import { useModal } from './components/react-modal/useModal';
 
 function App() {
+  const { isShown, toggle } = useModal();
+
+  const content = <React.Fragment>Hey, I'm a model.</React.Fragment>;
+
+
+  // const filterHarbors = harbaData.filter((hrba: any) => hrba.locale === "da_DK" )
+
+  console.log(harbaData)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MapContainer center={[55.721050, 11.723988]} zoom={8} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+
+      {harbaData.map((hrb: any) => (
+        <Marker 
+          key = {hrb.id}
+          position = {[hrb.lat, hrb.lon]}>
+          
+          <Popup position={[hrb.lat, hrb.lon]}>
+            <h2>{"Name: " + hrb.name}</h2>
+            <img className="thumb" alt="thumb" src={hrb.image} />
+            {/* <CurrentWeather /> */}
+             <button onClick={toggle}>Book</button>
+            <Modal isShown={isShown} hide={toggle} modalContent={content} headerText="Booking form" />
+          </Popup>
+        </Marker>
+      ))}
+     
+    </MapContainer>
   );
 }
 
